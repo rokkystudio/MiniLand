ab0f02e3bfda1df60caa586f1271fbbefd5b4d95
BuildTools <unconfigured@null.spigotmc.org>
2019-05-23 20:54:48 +0300

    Update modules

--- desktop/src/com/rokkystudio/nekoland/desktop/DesktopLauncher.java
+++ desktop/src/com/rokkystudio/nekoland/desktop/DesktopLauncher.java
@@ -6,6 +6,7 @@ import com.rokkystudio.nekoland.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		System.setProperty("user.name", "user");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Main(), config);
 	}
